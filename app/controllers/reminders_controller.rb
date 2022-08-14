dab0aaf47539fcf0946e060bf15af248e1d709d2
Mark Miranda <mmiranda@doximity.com>
2022-08-14 11:17:08 -0600

    MVP PUSH

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -8,7 +8,12 @@ class RemindersController < ApplicationController
   end
 
   def create
-    @reminder = Reminder.new(reminder_params)
+    @reminder = Reminder.new
+    Time.use_zone("America/Denver") do
+      @reminder.attributes = reminder_params
+      time_in_zone = Time.zone.local(reminder_params["run_at(1i)"], reminder_params["run_at(2i)"], reminder_params["run_at(3i)"], reminder_params["run_at(4i)"], reminder_params["run_at(5i)"])
+      @reminder.run_at = time_in_zone
+    end
     if @reminder.save
       flash[:alert] = "Your reminder has been created and scheduled!"
       TextSendingJob.perform_at(
